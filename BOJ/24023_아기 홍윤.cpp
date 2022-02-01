5040300387a6a60417660367c6ea8c1be094a648
TaeWan KIM <97300906+Waan2y@users.noreply.github.com>
2022-02-02 07:48:09 +0900

    Add comments :P
    TIC : Title is Content :)

--- BOJ/24023_아기 홍윤.cpp
+++ BOJ/24023_아기 홍윤.cpp
@@ -15,21 +15,21 @@ int main(){
 
   int s = 0;
   int ans = 0;
-  for(int i=0; i<N; i++){
+  for(int i=0; i<N; i++){   // 배열 만들기
     int tmp;
     cin >> tmp;
     A.push_back(tmp);
   }
 
   for(int i=0; i<N; i++){
-    if((~K) & A[i]){
-      s = i+1;
-      ans = 0;
+    if((~K) & A[i]){        // A[i]가 K에 없는 bit를 가지고 있는지
+      s = i+1;              // A[i]는 연속 구간에 시작이 될 수 없음
+      ans = 0;              // 현재까지 연속구간 answer 초기화
       continue;
     }
-    else{
-      ans |= A[i];
-      if(ans == K){
+    else{                   // A[i]는 K에 없는 bit가 없음
+      ans |= A[i];          // 현재 연속구간 anwer 에 OR
+      if(ans == K){         // answer ?
         cout << s+1 << ' ' << i+1 << '\n';
         return 0;
       }
